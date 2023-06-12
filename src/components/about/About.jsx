40fd6f7c54a3ddb906ea53395e2e7a29df8fcace
Jd Perez <admin@dtl360.com>
2023-06-12 11:47:32 -0500

    initial

---
+++ src/components/about/About.jsx
@@ -0,0 +1,14 @@
+//import React from 'react'
+import "./about.css"    
+
+export const About = () => {
+  return (
+   <section className="about container section" id="about">
+    <h2 className="section__title">About Me</h2>
+    <span className="section__subtitle">My introduction</span>
+   </section>
+  )
+}
+
+export default About
+
---
+++ src/components/home/HeaderSocial.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unknown-property */
+//import React from 'react'
+
+
+function HeaderSocial() {
+  return (
+    <div className='home__socials'>
+      <a href='#' className='home__socials-link' target='_blank'>
+      <i className="icon-social-facebook"></i>
+      </a>
+      <a href='#' className='home__socials-link'>
+      <i className="icon-social-twitter"></i>
+      </a>
+      <a href='#' className='home__socials-link'>
+      <i className="icon-social-instagram"></i>
+      </a>
+    </div>
+  )
+}
+
+export default HeaderSocial
