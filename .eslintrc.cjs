dfd4655666f47633d72a5bd2085686190af88174
Jd Perez <admin@dtl360.com>
2023-06-14 20:02:28 -0500

    add resume

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,14 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/module-exports": "off",
+    "react/underscore-dangle": "off",
   },
 }
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './resume.css';
+import Data from './Data';
+import Card from './Card';
 
 export const Resume = () => {
   return (
-    <div>Resume</div>
+    <section className="resume container section" id='resume'>
+      <h2 className="section__title">
+        Experiencia
+      </h2>
+      <div className="resume__container grid">
+        <div className="timeline grid">
+          {Data.map((item, id) => {
+            if (item.category === 'education') {
+              return (
+                <Card key={id} icon={item.icon} title={item.title} year={item.year} desc={item.desc} />
+              )
+            }
+          }
+          )
+          }
+        </div>
+        <div className="timeline grid">
+          {Data.map((item, id) => {
+            if (item.category === 'experience') {
+              return (
+                <Card key={id} icon={item.icon} title={item.title} year={item.year} desc={item.desc} />
+              )
+            }
+          })}        </div>
+
+      </div>
+    </section>
   )
 }
 
