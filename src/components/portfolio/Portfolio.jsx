d7b74d5a52c596ad7b60e0934c663611383a30b6
Jd Perez <admin@dtl360.com>
2023-06-18 11:18:23 -0500

    add works

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./portfolio.css";
+import Menu from "./Menu";
 export const Portfolio = () => {
+  
+  // eslint-disable-next-line no-unused-vars
+  const [items, setItems] = useState(Menu);
   return (
-    <div>Portfolio</div>
+    <section className="work container section" id='work'>
+    <h2 className="section__title">
+      Trabajos Recientes
+    </h2>
+    <div className="work__filters">
+      <span className="work__item" >Everything</span>
+      <span className="work__item" >Creative</span>
+      <span className="work__item" >Art</span>
+      <span className="work__item" >Desing</span>
+      <span className="work__item" >Branding</span>
+    </div>
+
+    <div className="work__container grid">
+      {items.map((item) => {
+        const {id, image, title, category} = item;
+        return(
+          <div className="work__card" key={id}>
+            <div className="work__thumbnail">
+              <img src={image} alt="" className='work__img' />
+              <div className="work__mask"></div>
+            </div>
+            <div className="work__category">{category}</div>
+            <h3 className="work__title">{title}</h3>
+            <a href="#" className="work__button">
+              <i className='icon-link work__button-icon'></i>
+            </a>
+          </div>
+        )
+        
+      })}
+    </div>
+    </section>
   )
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,39 @@
-//import React from 'react';
+import React,{useState, useEffect} from 'react';
 import "./sidebar.css";
 import logo from "../../assets/logo.svg";
 
 export const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState('');
+  const [activeSection, setActiveSection] = useState('');
+
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  }
+
+
+  const handleScroll = () => {
+    const sections = document.querySelectorAll('section'); // Cambia 'section' por el selector que identifica tus secciones
+    let currentSection = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (window.scrollY >= sectionTop - sectionHeight / 2) {
+        currentSection = section.id;
+      }
+    });
+
+    setActiveSection(currentSection);
+    setActiveItem('');
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+
   return (
     <aside className='aside'>
       <a href='#home' className='nav__logo'>
@@ -11,39 +42,39 @@ export const Sidebar = () => {
       <nav className='nav'>
         <div className='nav__menu'>
           <ul className='nav__list'>
-            <li className='nav__item'>
-              <a href='#home' className='nav__link'>
+            <li  className={`nav__item ${activeItem === 'home' || activeSection === 'home' ? 'active' : ''}`}>
+              <a href='#home' className='nav__link'  onClick={() => handleItemClick('home')}>
                <i className="icon-home"></i>
               </a>
             </li>
-            <li className='nav__item'>
-              <a href='#about' className='nav__link'>
+            <li  className={`nav__item ${activeItem === 'about'  || activeSection === 'about' ? 'active' : ''}`}>
+              <a href='#about' className='nav__link' onClick={() => handleItemClick('about')}>
                 <i className="icon-user-following"></i>
               </a>
             </li>
-            <li className='nav__item'>
-              <a href='#services' className='nav__link'>
+            <li className={`nav__item ${activeItem === 'services'   || activeSection === 'services' ? 'active' : ''}`}>
+              <a href='#services' className='nav__link' onClick={() => handleItemClick('services')}>
                <i className="icon-briefcase"></i>
               </a>
             </li>
-            <li className='nav__item'>
-              <a href='#resume' className='nav__link'>
+            <li className={`nav__item ${activeItem === 'resume' || activeSection === 'resume' ? 'active' : ''}`}>
+              <a href='#resume' className='nav__link' onClick={() => handleItemClick('resume')}>
                 <i className="icon-graduation"></i>
               </a>
             </li>
-            <li className='nav__item'>
-              <a href='#portfolio' className='nav__link'>
+            <li className={`nav__item ${activeItem === 'portfolio' || activeSection === 'portfolio' ? 'active' : ''}`}>
+              <a href='#portfolio' className='nav__link' onClick={() => handleItemClick('portfolio')}>
                 <i className="icon-layers"></i>
               </a>
             </li>
-            <li className='nav__item'>
-              <a href='#blog' className='nav__link'>
+            <li className={`nav__item ${activeItem === 'blog' || activeSection === 'blog' ? 'active' : ''}`}>
+              <a href='#blog' className='nav__link' onClick={() => handleItemClick('blog')}>
                 <i className="icon-note"></i>
               </a>
             </li>
-            <li className='nav__item'>
-              <a href='#contact' className='nav__link'>
-                <i className="icon-bubble"></i>
+            <li className={`nav__item ${activeItem === 'contact' || activeSection === 'contact' ? 'active' : ''}`}>
+              <a href='#contact' className='nav__link'  onClick={() => handleItemClick('contact')}>
+                <i className="icon-bubble" ></i>
               </a>
             </li>
           </ul>
