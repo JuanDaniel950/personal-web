fa702c9241035f806555ecbbad4b683f9361f723
Jd Perez <admin@dtl360.com>
2023-06-19 09:38:43 -0500

    bug fixed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import "./contact.css"
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="contact container section" id="contact">
+        <h2 className="section__title">Blog</h2>
+    </section>
   )
 }
 
